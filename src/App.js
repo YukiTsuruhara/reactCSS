850be61ddb8843f13273914723114fa83f53539e
YukiTsuruhara <81816095+YukiTsuruhara@users.noreply.github.com>
2021-05-16 15:49:24 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import {useState} from "react";
+import React from "react";
+import { StyledJsx } from "./components/Styled";
+import { StyledComponents } from "./components/StyledComponents";
+import { Emotion } from "./components/Emotion";
+// import { ChildArea } from "./ChildArea";
+import "./styles.css";
+export default function App() {
+  return (
+    <>
+      <StyledJsx />
+      <StyledComponents />
+      <Emotion />
+    </>
+  );
+}
